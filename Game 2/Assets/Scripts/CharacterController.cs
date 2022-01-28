e0e2751bfae0e7f3eae982008612f6acde6d88a8
Jackson <jvs8230@rit.edu>
2022-01-28 10:46:53 -0500

    Finished the player controller

--- Game 2/Assets/Scripts/CharacterController.cs
+++ Game 2/Assets/Scripts/CharacterController.cs
@@ -6,20 +6,30 @@ public class CharacterController : MonoBehaviour
 {
     public float speed = 10f;
     public float jumpForce = 10f;
-    private float movement;
+    public LayerMask groundMask;
+
+    private Rigidbody2D rb;
+    private Transform groundCheckTransform;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        groundCheckTransform = gameObject.transform.GetChild(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-       movement = Input.GetAxisRaw("Horizontal");
+        // gets the inputs we need
+        float movement = Input.GetAxisRaw("Horizontal");
+        bool jump = Input.GetButtonDown("Jump") && Physics2D.OverlapCircle(groundCheckTransform.position, 0.35f, groundMask);
+
+        // changes the player's velocity for movement and jumping
+        rb.velocity = rb.velocity + new Vector2(movement * speed - rb.velocity.x, jump ? jumpForce : 0);
+
+
     }
-    private void FixedUpdate()
-    {
-        transform.position += new Vector3(movement * speed * Time.deltaTime,0 ,0);
-    }
+
 }
