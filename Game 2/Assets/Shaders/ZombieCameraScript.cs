f83d504ea39cca2495c44b204eb3dcd1a198b517
Jackson <jvs8230@rit.edu>
2022-01-28 19:13:22 -0500

    Made a shader script

---
+++ Game 2/Assets/Shaders/ZombieCameraScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class ZombieCameraScript : MonoBehaviour
+{
+
+    public Material postMat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, postMat);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //postMat = new Material(postMat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
