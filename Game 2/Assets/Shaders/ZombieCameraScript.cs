a7b62d8e6789d773a26e2c68da445b0800f321ed
Jackson <jvs8230@rit.edu>
2022-01-28 21:10:12 -0500

    made the two levels display the different amounts of infection

--- Game 2/Assets/Shaders/ZombieCameraScript.cs
+++ Game 2/Assets/Shaders/ZombieCameraScript.cs
@@ -7,16 +7,18 @@ using UnityEngine;
 public class ZombieCameraScript : MonoBehaviour
 {
     public Material postMat;
+    public float infection;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        postMat.SetFloat("_Infection", infection);
         Graphics.Blit(source, destination, postMat);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        //postMat = new Material(postMat);
+        postMat = new Material(postMat);
 
     }
 
