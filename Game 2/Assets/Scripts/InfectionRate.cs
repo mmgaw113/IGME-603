f242c5821d4c74706672d3a43bac766bc3c39e6a
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2022-02-01 14:27:35 -0500

    Enemy and Infection Updates

--- Game 2/Assets/Scripts/InfectionRate.cs
+++ Game 2/Assets/Scripts/InfectionRate.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InfectionRate : MonoBehaviour
 {
     public float rate = 0.5f;
     public float currentInfection = 1;
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,7 @@ public class InfectionRate : MonoBehaviour
         if(currentInfection < 100)
         {
             currentInfection += rate * Time.deltaTime;
+            text.text = "Inefection: " + Mathf.Round(currentInfection) + "%";
         }
         else
         {
