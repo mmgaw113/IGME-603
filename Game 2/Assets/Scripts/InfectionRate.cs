da050255c3d9ebcf168c5a2da9a2211339f8c55f
Candygonemad <candygonemad@gmail.com>
2022-02-06 16:38:57 -0500

    UI

--- Game 2/Assets/Scripts/InfectionRate.cs
+++ Game 2/Assets/Scripts/InfectionRate.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InfectionRate : MonoBehaviour
 {
     public float rate = 0.5f;
     public float currentInfection = 1;
     public TextMeshProUGUI text;
+    private Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        text= GameObject.Find("HealthUI").GetComponent<TextMeshProUGUI>();
+        //text= GameObject.Find("HealthUI").GetComponent<TextMeshProUGUI>();
+        slider = GameObject.Find("Canvas").GetComponentInChildren<Slider>();
+        Debug.Log("Slider: " + slider);
     }
 
     // Update is called once per frame
@@ -21,7 +25,8 @@ public class InfectionRate : MonoBehaviour
         if(currentInfection < 100)
         {
             currentInfection += rate * Time.deltaTime;
-            text.text = "Inefection: " + Mathf.Round(currentInfection) + "%";
+            slider.value = currentInfection;
+            //text.text = "Inefection: " + Mathf.Round(currentInfection) + "%";
         }
         else
         {
